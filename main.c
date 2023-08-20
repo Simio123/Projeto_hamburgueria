524c5c368bf911a1cc154d8fc8d9de3219aa5929
Emerson <emerson.parelhasrn@hotmail.com>
2023-08-20 14:37:39 -0300

    projeto v1

--- main.c
+++ main.c
@@ -1,9 +1,52 @@
+/******************************************************************************
+***             Universidade Federal do Rio Grande do Norte                 ***
+***             Centro de Ensino Superior do Seridó                         ***
+***             Departamento de Computação e Tecnologia                     ***
+***             Disciplina: Programação                                     ***
+***             Professor: Flavius Gorgônio                                 ***
+***             Projeto: Sistema de Gestão de Hamburgueria                  ***
+***             Aluno: Emerson da Silva Santos                              ***
+***             Linguagem: C                                                ***
+*******************************************************************************/
+
+/* Bibliotecas */
 #include<stdio.h>
 #include<stdlib.h>
 
+/* Funções de exibição de telas */
+
+/* Telas principal*/
+void menu_principal(void);
+
+void menu_pedidos(void);
+void menu_funcionarios(void);
+
+void informacoes(void);
+
+/* Telas de cadastramento */
+
+/* Telas de cadastramento */
+void tela_informacoes(void);
+void tela_informacoes(void);
+void tela_informacoes(void);
+void tela_informacoes(void);
+void tela_informacoes(void);
+
+
+/* Telas de descadastramento */
+
+/* Telas de exibição */
+
+/* Telas principais */
+
+/* Telas principais */
+
+/* Telas principais */
 int main(void){
 
-    printf("asda");
+    printf("Insira um numero  \n");
+
+
 
     return 0;
 }
