e2ff9db30b7feb88baf954387ae327706dc2cf22
Emerson <emerson.parelhasrn@hotmail.com>
2023-08-20 15:05:06 -0300

    projeto v1

--- main.c
+++ main.c
@@ -26,20 +26,29 @@ void promover_funcionario(void);
 void rebaixar_funcionario(void);
 void informacoes_funcionario(void);
 
+/* Telas de mesas */
+void menu_mesas(void);
+void adicionar_mesa(void);
+void retirar_mesa(void);
+void informacoes_mesas(void);
+
+/* Telas de pedidos */
+void menu_pedidos(void);
+void tipo_pedido(void);
+void cadastrar_pedido(void);
+void cancelar_pedido(void);
+void informacoes_pedido(void);
 
 /* Funções auxiliares*/
-
 void valida_cpf(void);
 void salva_arquivos(void);
 void recupera_arquivos(void);
-
-
-
-
+void mensagem_erros_numericos(void);
+void mensagem_erros_caracteres(void);
+void emissao_nota_fiscal(void);
 
 int main(void){
 
-    printf("Insira um numero  \n");
 
 
 
