ca3d6c1999f473d7a84db89857f08b85eee2f609
Emerson Santos <134620060+Simio123@users.noreply.github.com>
2023-09-20 16:12:10 -0300

    Update telas.h

--- telas.h
+++ telas.h
@@ -2,15 +2,26 @@
 #define TELAS_H
 
 enum Tela {
-	// Telas iniciais
+    // Telas iniciais
     TELA_INICIAL,
     TELA_LOGIN,
     TELA_INFO_PROJETO,
     TELA_INFO_EQUIPE,
+
+    // Telas secundárias 
+    TELA_ADMINISTRADOR,
+    TELA_FUNCIONARIO,
+    TELA_CLIENTE,
+
+    // Telas terciarias
+    TELA_ESTOQUE,
+    TELA_PEDIDOS,
+    TELA_RELATORIOS,
+    TELA_DELIVERY,
+    TELA_OPCOES_DE_PAGAMENTO,
+
     // Easter eggs
     BEIJO,
-    // Telas secundárias 
-    
 };
 
 struct Pilha;
@@ -25,6 +36,18 @@ void tela_inicial(int opcao, struct Pilha* pilha);
 void tela_login(int opcao, struct Pilha* pilha);
 void tela_info_projeto(int opcao, struct Pilha* pilha);
 void tela_info_equipe(int opcao, struct Pilha* pilha);
+
+// Telas secundárias 
+void administrador(int opcao, struct Pilha* pilha);
+void funcionario(int opcao, struct Pilha * pilha);
+void cliente(int opcao, struct Pilha * pilha);
+
+// Telas terciarias
+void estoque(int opcao, struct Pilha * pilha);
+void pedidos(int opcao, struct Pilha * pilha);
+void relatorios(int opcao, struct Pilha * pilha);
+void delivery(int opcao, struct Pilha * pilha);
+void opcoes_de_pagamento(int opcao, struct Pilha * pilha);
 
 // Easter eggs
 void beijo(int opcao, struct Pilha* pilha);
